327bc47eeee0d422c59deda7ab126e86f55145fd
Javad Bayzavi <javadbayzavi@gmail.com>
2021-01-13 13:25:36 +0330

    do some business coding

--- services/Models/progresi_users.cs
+++ services/Models/progresi_users.cs
@@ -11,5 +11,9 @@ namespace services.Models
         public string name { get; set; }
         public string family { get; set; }
         public string username { get; set; }
+        public int authmode { get; set; }
+        public int last_access { get; set; }
+        public int create_date { get; set; }
+        public int status { get; set; }
     }
 }
