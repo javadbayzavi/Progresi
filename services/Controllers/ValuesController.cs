c214f82628215d106a725a3ec7cbb51fc0f97106
Javad Bayzavi <javadbayzavi@gmail.com>
2021-01-18 12:29:46 +0330

    Db Context update

--- services/Controllers/ValuesController.cs
+++ services/Controllers/ValuesController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace service.Controllers
+namespace services.Controllers
 {
     public class ValuesController : ApiController
     {
---
+++ services/Models/BunchdbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using services.Models;
+
+namespace services.Models
+{
+    public class BunchdbContext: DbContext
+    {
+        public DbSet<> MyProperty { get; set; }
+        public BunchdbContext():base("")
+            {
+
+        }
+    }
+}
