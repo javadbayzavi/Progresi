640a066a1bd943cd05c53edfd0d3ba0ea5208a0e
Javad Bayzavi <javadbayzavi@gmail.com>
2021-01-24 13:40:42 +0330

    integration with mongodb

--- services/Models/progresiDataProxy.cs
+++ services/Models/progresiDataProxy.cs
@@ -1,15 +1,31 @@
 namespace services.Models
 {
     using System;
-    using System.Data.Entity;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Linq;
-
-    public partial class progresiDataProxy : DbContext
+    using MongoDB.Driver;
+    using MongoDB.EntityFramework.Core;
+`    public class MongoSettings
     {
-        public progresiDataProxy()
-            : base("name=progresidb")
+        public MongoSettings(string connectionString, string databaseName)
         {
+            ConnectionString = connectionString;
+            DatabaseName = databaseName;
+        }
+
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+
+    public class progresiDataProxy : MongoDB.EntityFramework.Core.DbContext
+    {
+        public progresiDataProxy(IMongoClient client, MongoSettings settings)
+            : base(client, settings.DatabaseName)
+        {
+            //TODO: lazy
+
         }
 
         public virtual DbSet<progresi_bunches> progresi_bunches { get; set; }
@@ -18,17 +34,12 @@ namespace services.Models
         public virtual DbSet<progresi_tasks> progresi_tasks { get; set; }
         public virtual DbSet<progresi_teams> progresi_teams { get; set; }
         public virtual DbSet<progresi_users> progresi_users { get; set; }
-        
+
         public virtual DbSet<progresi_stage_tasks> progresi_stage_tasks { get; set; }
         public virtual DbSet<progresi_bunch_categories> progresi_bunch_categories { get; set; }
         public virtual DbSet<progresi_task_assigns> progresi_task_assigns { get; set; }
         public virtual DbSet<progresi_task_categories> progresi_task_categories { get; set; }
         public virtual DbSet<progresi_user_teams> progresi_user_teams { get; set; }
-        
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            Database.SetInitializer<progresiDataProxy>(null);
-        }
 
     }
 }
